4dc59df66e34cbebe49c5f3c7425c9eadf820976
Philippa Borrill <p.borrill@gmail.com>
2016-11-03 14:21:19 +0000

    final filtering settings to get high conf variants

--- filter_starch_genes_02_11_2016.sh
+++ filter_starch_genes_02_11_2016.sh
@@ -28,17 +28,33 @@ srun java -jar /usr/users/metbio/borrillp/bin/snpEff/SnpSift.jar intIdx freebaye
 srun cat freebayes_all_samples_together_whease_only_starch_gene_variants.vcf | java -jar /usr/users/metbio/borrillp/bin/snpEff/SnpSift.jar filter "(countHet() < 2)" > freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets.vcf
 
 # only keep sites with hom dp >= 3 or het dp >= 5
-srun cat freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets.vcf | java -jar /usr/users/metbio/borrillp/bin/snpEff/SnpSift.jar filter "(countHet() > 0) && (DP >= 5) | (countHom() > 0) && (DP >= 3)" >  freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_homCov3_hetCov5.vcf
+#srun cat freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets.vcf | java -jar /usr/users/metbio/borrillp/bin/snpEff/SnpSift.jar filter "((countHet() > 0) && (DP >= 5)) | ((countHom() > 0) && (DP >= 3))" >  freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_homCov3_hetCov5.vcf
 
 # only keep sites with 75 % of samples with genotype
-srun vcftools --vcf freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_homCov3_hetCov5.vcf --out freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_homCov3_hetCov5_75perc_indiv_cov --max-missing 0.75  --recode --recode-INFO-all
+#srun vcftools --vcf freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_homCov3_hetCov5.vcf --out freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_homCov3_hetCov5_75perc_indiv_cov --max-missing 0.75  --recode --recode-INFO-all
+
+# only keep sites with hom dp >= 3 or het dp >= 5
+#srun cat freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets.vcf | java -jar /usr/users/metbio/borrillp/bin/snpEff/SnpSift.jar filter "(isHom( GEN[*]) & GEN[*].DP >= 3) | (isHet( GEN[*]) & GEN[*].DP >= 5) " >  freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_homCov3_hetCov5_v2.vcf
+
+# only keep sites with 75 % of samples with genotype
+#srun vcftools --vcf freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_homCov3_hetCov5_v2.vcf --out freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_homCov3_hetCov5_v2_75perc_indiv_cov --max-missing 0.75  --recode --recode-INFO-all
+
+# only keep sites with DP>=3 
+srun vcftools --vcf freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets.vcf --out freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_dp3 --minDP 3  --recode --recode-INFO-all
+
+# only keep sites 75 % genotype
+srun vcftools --vcf freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_dp3.recode.vcf --out freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_dp3_0.75_perc --max-missing 0.75  --recode --recode-INFO-all
+
+# only keeps sites with >0 variants but ignore read depth
+srun cat freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_dp3_0.75_perc.recode.vcf | java -jar /usr/users/metbio/borrillp/bin/snpEff/SnpSift.jar filter "(countVariant() > 0)" > freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_dp3_0.75_perc_variant_sites.vcf
+
 
 # or just filter for low coverage hets (require >=2 min depth for alt allele in hets)
 # only keep sites with het AO (alt allele depth) >= 2
-srun cat freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets.vcf | java -jar /usr/users/metbio/borrillp/bin/snpEff/SnpSift.jar filter "(countHet() > 0) && (AO >= 2) | (countHom() > 0)" >  freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_hetAltCov2.vcf
+#srun cat freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets.vcf | java -jar /usr/users/metbio/borrillp/bin/snpEff/SnpSift.jar filter "(countHet() > 0) && (AO >= 2) | (countHom() > 0)" >  freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_hetAltCov2.vcf
 
 # then filter for sites with 75 % with genotype
-srun vcftools --vcf freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_hetAltCov2.vcf --out freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_hetAltCov2_75perc_indiv_cov --max-missing 0.75  --recode --recode-INFO-all
+#srun vcftools --vcf freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_hetAltCov2.vcf --out freebayes_all_samples_together_whease_only_starch_gene_variants_under_2_hets_hetAltCov2_75perc_indiv_cov --max-missing 0.75  --recode --recode-INFO-all
 
 
 
